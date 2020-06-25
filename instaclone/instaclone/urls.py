f2377f7c13d10528a9b2c7ff3d71ab72b14c8500
Thendo Rambane <rambane.t.r@gmail.com>
2020-06-25 16:49:56 +0200

    Misc Bugs

--- instaclone/instaclone/urls.py
+++ instaclone/instaclone/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import re_path, include, path
-from rest_framework.authtoken import views
+from rest_framework.authtoken import views as auth_views
 from instaclone import views
 
 urlpatterns = [
     re_path(r'', include('userprofile.urls')),
-    path(r'api/login/', views.obtain_auth_token, name='login')
-    path(r'api/logout/', views.logout, name='logout')
+    path(r'api/login/', auth_views.obtain_auth_token, name='login'),
+    path(r'api/logout/', views.logout, name='logout'),
 ]
--- instaclone/instaclone/views.py
+++ instaclone/instaclone/views.py
@@ -1,11 +1,12 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 
 @api_view()
-@permission_classes([permissions.IsAuthenticated])
+@permission_classes([IsAuthenticated])
 def logout(request):
     request.user.auth_token.delete()
     return Response(status=status.HTTP_200_OK, data={'logged out'})
