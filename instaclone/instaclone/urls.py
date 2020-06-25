9f1d90a76ddb635e435d0e746cc76a1bf2875475
Thendo Rambane <rambane.t.r@gmail.com>
2020-06-25 16:39:33 +0200

    Move login to top level call

--- instaclone/instaclone/urls.py
+++ instaclone/instaclone/urls.py
@@ -4,4 +4,5 @@ from django.urls import re_path, include
 
 urlpatterns = [
     re_path(r'', include('userprofile.urls')),
+    path(r'api/login/', views.obtain_auth_token, name='login')
 ]
--- instaclone/userprofile/urls.py
+++ instaclone/userprofile/urls.py
@@ -1,7 +1,6 @@
 from rest_framework import routers
 from .views import ProfileViewSet
 from django.urls import path, include, re_path
-from rest_framework.authtoken import views
 
 
 router = routers.SimpleRouter()
@@ -9,8 +8,4 @@ router.register(r'api/profiles', ProfileViewSet, 'profiles')
 
 urlpatterns = [
     path('', include(router.urls)),
-]
-
-urlpatterns += [
-    path('api/profiles/login', views.obtain_auth_token, name='login')
 ]
