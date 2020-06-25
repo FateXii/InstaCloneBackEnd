1bcc48a2e6036da9ddf2a4d3b2a64992dd12382f
Thendo Rambane <rambane.t.r@gmail.com>
2020-06-25 13:16:58 +0200

    Clean up main urls.py

--- instaclone/instaclone/urls.py
+++ instaclone/instaclone/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import re_path, include
 
-urlpatterns = [
 
-    # re_path(r'', include('comments.urls')),
+urlpatterns = [
     re_path(r'', include('userprofile.urls')),
-    # re_path(r'', include('posts.urls')),
 ]
